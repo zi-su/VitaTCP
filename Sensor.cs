e9a50911002f77958f5beaa5e5300207b8f43d3e
Masato Naito <aitona765@gmail.com>
2014-07-25 03:50:38 +0900

    Revert 93e6bb6..0f77ad5
    This rolls back to commit 93e6bb661b1615e871b82dabe1df5bdbfe75a8cb.

--- Sensor.cs
+++ Sensor.cs
@@ -1,63 +1,44 @@
 ﻿using UnityEngine;
+using UnityEngine.PSM;
 using System.Collections;
 using System.Net;
 using System.Net.Sockets;
 using System;
 using System.Threading;
 public class Sensor : MonoBehaviour {
-
-	enum BUTTON{
-		RIGHT_DOWN,
-		RIGHT_RIGHT,
-		RIGHT_LEFT,
-		RIGHT_UP,
-		LEFT_SHOULDER,
-		RIGHT_SHOULDER,
-		CENTER_RIGHT,
-		CENTER_LEFT,
-		LEFT_UP,
-		LEFT_DOWN,
-		LEFT_LEFT,
-		LEFT_RIGHT,
-		MAX_BUTTON,
-	};
-
-	struct SensorData{
-		public Vector3 accelation;
-		public int touches;
-		public Vector2 leftStick;
-		public Vector2 rightStick;
-		public bool[] buttons;
-	};
-
-	SensorData data;
+	VitaSensorData.Data data;
 	public GameObject touch;
 	public GameObject gyro;
 	public GameObject dataText;
 	public GameObject guiText;
+	public Rect listenButton;
+	public Rect listenStopButton;
+	public Rect textRect;
+	public RenderTexture renderTex;
+	public int port;
+	public string iptext;
 	IPAddress[] address;
 	TcpListener listen;
 	TcpClient client = null;
 	NetworkStream ns;
 	Thread thread;
-	public Rect listenButton;
-	public Rect listenStopButton;
+
 	private bool isWrite = false;
 
 	private TouchScreenKeyboard keyboard;
-	public Rect textRect;
-	private string iptext;
-	public RenderTexture renderTex;
+
+
+
 	// Use this for initialization
 	void Start () {
-		data.buttons = new bool[(int)BUTTON.MAX_BUTTON];
+		Input.compass.enabled = true;
+		data.buttons = new bool[(int)VitaSensorData.BUTTON.MAX_BUTTON];
 		string hostname = Dns.GetHostName ();
 		address = Dns.GetHostAddresses (hostname);
 		gyro.GetComponent<GUIText>().text = address[0].ToString();
 		foreach (IPAddress ip in address) {
-			dataText.GetComponent<GUIText>().text += ip.ToString() + "\n";
+			dataText.GetComponent<GUIText>().text += ("\n" + ip.ToString() + "\n");
 		}
-		iptext = "";
 	}
 
 	void acceptCallback(IAsyncResult result){
@@ -70,46 +51,48 @@ public class Sensor : MonoBehaviour {
 	}
 
 	void TCPRoutine(){
-			gyro.GetComponent<GUIText>().text = Input.gyro.userAcceleration.ToString();
-			touch.GetComponent<GUIText> ().text = Input.touches.Length.ToString ();
-			data.accelation = Input.gyro.userAcceleration;
-			data.touches = Input.touches.Length;
-			data.leftStick.x = Input.GetAxis ("Horizontal");
-			data.leftStick.y = Input.GetAxis ("Vertical");
-			data.rightStick.x = Input.GetAxis ("RightStickHorizontal");
-			data.rightStick.y = Input.GetAxis ("RightStickVertical");
-			data.buttons [(int)BUTTON.RIGHT_DOWN] = Input.GetKey (KeyCode.JoystickButton0);
-			data.buttons [(int)BUTTON.RIGHT_RIGHT] = Input.GetKey (KeyCode.JoystickButton1);
-			data.buttons [(int)BUTTON.RIGHT_LEFT] = Input.GetKey (KeyCode.JoystickButton2);
-			data.buttons [(int)BUTTON.RIGHT_UP] = Input.GetKey (KeyCode.JoystickButton3);
-			data.buttons [(int)BUTTON.LEFT_SHOULDER] = Input.GetKey (KeyCode.JoystickButton4);
-			data.buttons [(int)BUTTON.RIGHT_SHOULDER] = Input.GetKey (KeyCode.JoystickButton5);
-			data.buttons [(int)BUTTON.CENTER_LEFT] = Input.GetKey (KeyCode.JoystickButton6);
-			data.buttons [(int)BUTTON.CENTER_RIGHT] = Input.GetKey (KeyCode.JoystickButton7);
-			data.buttons [(int)BUTTON.LEFT_UP] = Input.GetKey (KeyCode.JoystickButton8);
-			data.buttons [(int)BUTTON.LEFT_RIGHT] = Input.GetKey (KeyCode.JoystickButton9);
-			data.buttons [(int)BUTTON.LEFT_DOWN] = Input.GetKey (KeyCode.JoystickButton10);
-			data.buttons [(int)BUTTON.LEFT_LEFT] = Input.GetKey (KeyCode.JoystickButton11);
-			
-			guiText.GetComponent<GUIText> ().text = ns.DataAvailable.ToString ();
-			if (client != null && client.Connected && ns.DataAvailable == true) {
-				ns.ReadByte();
-				ns.Write (BitConverter.GetBytes(data.touches), 0, sizeof(Int32));
-				ns.Write (BitConverter.GetBytes(data.accelation.x), 0, sizeof(float));
-				ns.Write (BitConverter.GetBytes(data.accelation.y), 0, sizeof(float));
-				ns.Write (BitConverter.GetBytes(data.accelation.z), 0, sizeof(float));
-				ns.Write (BitConverter.GetBytes(data.leftStick.x), 0, sizeof(float));
-				ns.Write (BitConverter.GetBytes(data.leftStick.y), 0, sizeof(float));
-				ns.Write (BitConverter.GetBytes(data.rightStick.x), 0, sizeof(float));
-				ns.Write (BitConverter.GetBytes(data.rightStick.y), 0, sizeof(float));
-				foreach(bool b in data.buttons){
-					ns.Write (BitConverter.GetBytes(b), 0, sizeof(bool));
-				}
-				ns.Flush();
-				dataText.GetComponent<GUIText> ().text = ns.DataAvailable.ToString();
-				isWrite = true;
+		gyro.GetComponent<GUIText>().text = Input.gyro.userAcceleration.ToString();
+		touch.GetComponent<GUIText> ().text = Input.touches.Length.ToString ();
+		data.gyroAttitude = Input.gyro.attitude;
+		data.acceleration = Input.gyro.userAcceleration;
+		data.touches = Input.touches.Length;
+		data.leftStick.x = Input.GetAxis ("Horizontal");
+		data.leftStick.y = Input.GetAxis ("Vertical");
+		data.rightStick.x = Input.GetAxis ("RightStickHorizontal");
+		data.rightStick.y = Input.GetAxis ("RightStickVertical");
+		data.backTouches = PSMInput.touchesSecondary.Length;
+		data.compass = Input.compass.rawVector;
+
+		data.buttons [(int)VitaSensorData.BUTTON.RIGHT_DOWN] = Input.GetKey (KeyCode.JoystickButton0);
+		data.buttons [(int)VitaSensorData.BUTTON.RIGHT_RIGHT] = Input.GetKey (KeyCode.JoystickButton1);
+		data.buttons [(int)VitaSensorData.BUTTON.RIGHT_LEFT] = Input.GetKey (KeyCode.JoystickButton2);
+		data.buttons [(int)VitaSensorData.BUTTON.RIGHT_UP] = Input.GetKey (KeyCode.JoystickButton3);
+		data.buttons [(int)VitaSensorData.BUTTON.LEFT_SHOULDER] = Input.GetKey (KeyCode.JoystickButton4);
+		data.buttons [(int)VitaSensorData.BUTTON.RIGHT_SHOULDER] = Input.GetKey (KeyCode.JoystickButton5);
+		data.buttons [(int)VitaSensorData.BUTTON.CENTER_LEFT] = Input.GetKey (KeyCode.JoystickButton6);
+		data.buttons [(int)VitaSensorData.BUTTON.CENTER_RIGHT] = Input.GetKey (KeyCode.JoystickButton7);
+		data.buttons [(int)VitaSensorData.BUTTON.LEFT_UP] = Input.GetKey (KeyCode.JoystickButton8);
+		data.buttons [(int)VitaSensorData.BUTTON.LEFT_RIGHT] = Input.GetKey (KeyCode.JoystickButton9);
+		data.buttons [(int)VitaSensorData.BUTTON.LEFT_DOWN] = Input.GetKey (KeyCode.JoystickButton10);
+		data.buttons [(int)VitaSensorData.BUTTON.LEFT_LEFT] = Input.GetKey (KeyCode.JoystickButton11);
+		Debug.Log (data.compass + "backToches " + data.backTouches);
+		if (client != null && client.Connected && ns != null && ns.DataAvailable == false) {
+			ns.Write (BitConverter.GetBytes(data.touches), 0, sizeof(Int32));
+			ns.Write (BitConverter.GetBytes(data.acceleration.x), 0, sizeof(float));
+			ns.Write (BitConverter.GetBytes(data.acceleration.y), 0, sizeof(float));
+			ns.Write (BitConverter.GetBytes(data.acceleration.z), 0, sizeof(float));
+			ns.Write (BitConverter.GetBytes(data.leftStick.x), 0, sizeof(float));
+			ns.Write (BitConverter.GetBytes(data.leftStick.y), 0, sizeof(float));
+			ns.Write (BitConverter.GetBytes(data.rightStick.x), 0, sizeof(float));
+			ns.Write (BitConverter.GetBytes(data.rightStick.y), 0, sizeof(float));
+			foreach(bool b in data.buttons){
+				ns.Write (BitConverter.GetBytes(b), 0, sizeof(bool));
 			}
+			ns.Flush();
+			dataText.GetComponent<GUIText> ().text = ns.DataAvailable.ToString();
+			isWrite = true;
 		}
+	}
 	void OnGUI(){
 		if (GUI.Button (listenButton, "ListenStart")) {
 			listen = new TcpListener (IPAddress.Parse(iptext), 1234);
@@ -125,7 +108,6 @@ public class Sensor : MonoBehaviour {
 		if (keyboard != null) {
 			iptext = keyboard.text;
 		}
-		//}
 	}
 
 	void CloseTCP(){
